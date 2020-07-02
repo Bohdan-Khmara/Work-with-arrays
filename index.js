2fc80ef5acab0745b60bf91edef02c61f6df3795
Bohdan Khmara <Khmarabohdan@gmail.com>
2020-07-02 20:43:48 +0300

    Work with splice. Task 10

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
- * Work with splice. Task 9
+ * Work with splice. Task 10
  * @type {number[]}
  */
 let arr = [1, 2, 3, 4, 5];
-let removed = arr.splice(1, 2);
-alert(arr);
+let removed = arr.splice(1, 3);
+alert(removed);
