f360cd1683869213dbdd9cb78c523ca2b36dcd77
Bohdan Khmara <Khmarabohdan@gmail.com>
2020-07-02 20:41:38 +0300

    Work with slice. Task 8

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
- * Work with slice. Task 7
+ * Work with slice. Task 8
  * @type {number[]}
  */
 let arr = [1, 2, 3, 4, 5];
-let arr2 = arr.slice(0, 3);
+let arr2 = arr.slice(-2);
 alert(arr2);
